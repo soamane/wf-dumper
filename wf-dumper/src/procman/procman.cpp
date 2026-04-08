ddd7f5e7190ae5a868eb16e8226942e5e4615dc8
soamane <143885288+soamane@users.noreply.github.com>
2026-04-08 16:41:38 +0300

    ProcMan implementation

---
+++ wf-dumper/src/procman/procman.cpp
@@ -0,0 +1,104 @@
+﻿#include "procman.hpp"
+
+#include <TlHelp32.h>
+#include <windows.h>
+#include <print>
+
+ProcMan::ProcMan(const std::string& procName)
+    : procId(0), hProc(0), hModule(0), procName(procName) {}
+
+ProcMan::~ProcMan() {
+  if (hProc) {
+    CloseHandle(hProc);
+  }
+}
+
+bool ProcMan::Init() {
+  if (!GetProcId()) {
+    std::println("[-] Failed to get procId for '{}'", procName);
+    return false;
+  }
+
+  if (!GetProcBase()) {
+    std::println("[-] Failed to get procBase for '{}'", procName);
+    return false;
+  }
+
+  if (!OpenProc()) {
+    std::println("[-] Failed to open process '{}' with procId [{}]", procName,
+                 procId);
+    return false;
+  }
+
+  return true;
+}
+
+HANDLE ProcMan::GetHandle() const {
+  return hProc;
+}
+
+HMODULE ProcMan::GetModule() const {
+  return hModule;
+}
+
+std::size_t ProcMan::GetImageSize() const {
+  IMAGE_DOS_HEADER dos;
+  if (!ReadProcessMemory(hProc, hModule, &dos, sizeof(dos), nullptr)) {
+    return 0;
+  }
+
+  IMAGE_NT_HEADERS nt;
+  if (!ReadProcessMemory(
+          hProc, reinterpret_cast<std::uint8_t*>(hModule) + dos.e_lfanew, &nt,
+          sizeof(nt), nullptr)) {
+    return 0;
+  }
+
+  return nt.OptionalHeader.SizeOfImage;
+}
+
+bool ProcMan::OpenProc() {
+  hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+  return hProc != NULL;
+}
+
+bool ProcMan::GetProcId() {
+  auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  if (snapshot == INVALID_HANDLE_VALUE) {
+    return false;
+  }
+
+  PROCESSENTRY32 procEntry;
+  procEntry.dwSize = sizeof(PROCESSENTRY32);
+
+  if (Process32First(snapshot, &procEntry)) {
+    do {
+      if (procName == procEntry.szExeFile) {
+        procId = procEntry.th32ProcessID;
+        CloseHandle(snapshot);
+        return true;
+      }
+    } while (Process32Next(snapshot, &procEntry));
+  }
+
+  CloseHandle(snapshot);
+  return false;
+}
+
+bool ProcMan::GetProcBase() {
+  auto snapshot =
+      CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+  if (snapshot == INVALID_HANDLE_VALUE) {
+    return false;
+  }
+
+  MODULEENTRY32 modEntry;
+  modEntry.dwSize = sizeof(MODULEENTRY32);
+
+  if (Module32First(snapshot, &modEntry)) {
+    hModule = modEntry.hModule;
+    return true;
+  }
+
+  return false;
+}
---
+++ wf-dumper/src/procman/procman.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#ifndef PROCMAN_HPP
+#define PROCMAN_HPP
+
+#include <windows.h>
+#include <string>
+
+class ProcMan {
+ public:
+  ProcMan(const std::string& procName);
+  ~ProcMan();
+
+ public:
+  bool Init();
+
+ public:
+  HANDLE GetHandle() const;
+  HMODULE GetModule() const;
+  std::size_t GetImageSize() const;
+
+ private:
+  bool OpenProc();
+  bool GetProcId();
+  bool GetProcBase();
+
+ private:
+  DWORD procId;
+  HANDLE hProc;
+  HMODULE hModule;
+  std::string procName;
+};
+
+#endif  // !PROCMAN_HPP
