58ca65a0b755473c8c728d3e987958b01b3b97ae
soamane <143885288+soamane@users.noreply.github.com>
2026-04-11 01:03:28 +0300

    example

--- wf-dumper/src/main.cpp
+++ wf-dumper/src/main.cpp
@@ -3,17 +3,25 @@
 #include <scanner/scanner.hpp>
 
 int main() {
-  ProcMan proc("CrackMeEasy.exe");
+  ProcMan proc("Game.exe");
   if (!proc.Init()) {
     std::println("[-] Failed to initialize process manager");
     return -1;
   }
 
   PatternScanner scanner(proc);
-// E8 ? ? ? ? 48 8D 15 ? ? ? ? 48 83 3D
-  std::print("{:016X}",
-             scanner.GetPatternAddr(
-                 "\xE8\x00\x00\x00\x00\x48\x8D\x15\x00\x00\x00\x00\x48\x83\x3D",
-                 "x????xxx????xxx"));
+
+  uintptr_t firstadr = scanner.GetPatternAddr(
+      "\x48\xBD\x00\x00\x00\x00\x00\x00\x00\x00\x49\xBE\x00\x00\x00\x00\x00\x00"
+      "\x00\x00\x48\x89\x00\x48\x89\x40\x00\x48\x89\x40\x00\x66\xC7\x40\x00\x00"
+      "\x00\x48\x89\x43",
+      "xx????????xx????????xxxxxx?xxx?xxx???xxx");
+
+  uintptr_t secondadr = scanner.GetPatternAddr(
+      "\x49\xBE\x00\x00\x00\x00\x00\x00\x00\x00\x48\x89\x00\x48\x89\x40\x00\x48\x89\x40\x00\x66\xC7\x40\x00\x00\x00\x48\x89\x43",
+      "xx????????xxxxxx?xxx?xxx???xxx");
+  
+  std::println("{:016X}", scanner.GetImmediateVal<uintptr_t>(firstadr));
+  std::println("{:016X}", scanner.GetImmediateVal<uintptr_t>(secondadr));
   return 0;
 }
